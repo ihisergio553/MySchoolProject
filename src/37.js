b99145222cbe4bdb1b73bf2a020cbd648baa4e24
ihisergio553 <ihisergio553@gmail.com>
2025-05-17 08:51:33 +0000

    Add 37.js

---
+++ src/37.js
@@ -0,0 +1,8 @@
+// In this example, we will create an array of students and their corresponding grades.
+var studentGrades = [
+    { name: 'Alice', grade: 90 },
+    { name: 'Bob', grade: 85 },
+    { name: 'Charlie', grade: 75 },
+    { name: 'David', grade: 60 },
+    { name: 'Eve', grade: 55 }
+];
